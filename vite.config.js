6b7cfaf92f2d35a645d0a3523f1ec73fc079bdfb
madanbio <madanuideveloper@gmail.com>
2026-01-06 16:27:32 +0530

    Build output for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // ✅ for username.github.io repo
+  base: "/madanprofile/",
+  build: {
+    outDir: "docs",   // 👈 GitHub Pages friendly
+  },
 });
