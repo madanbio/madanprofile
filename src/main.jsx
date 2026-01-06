0e31210847e5c6b3fed43e67e09bad4beb250ad8
madanbio <madanuideveloper@gmail.com>
2026-01-06 15:38:48 +0530

    fix github pages blank page

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./assets/css/base7dd3.css";
-import "./assets/css/magnific7dd3.css";
-import "./assets/css/animated-headlines7dd3.css";
-import "./assets/css/style7dd3.css";
-import "./assets/js/jquery7dd3.js";
-import "./assets/js/isotope.js";
-import "./assets/js/magnific.js";
-import "./assets/js/animated-headlines7dd3.js";
-import "./assets/js/waypoints7dd3.js";
-import "./assets/js/app.js";
-import "./assets/js/init7dd3.js";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
